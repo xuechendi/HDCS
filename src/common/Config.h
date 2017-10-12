c3196adc3cd228c1566b1c9afa92e0558e9711c2
Chendi.Xue <chendi.xue@intel.com>
2017-10-12 13:10:49 +0800

    Add HDCSController above HDCSCore to make process
    1. fixed partial write bug in hdcs core
    2. add HDCSController who is a resource manager. and HDCS is main
    3. add a rough messenger
    4. add a test tool, do test on read/write/flush_all/promote_all
    
    Signed-off-by: Chendi.Xue <chendi.xue@intel.com>

--- src/common/Config.h
+++ src/common/Config.h
@@ -4,6 +4,7 @@
 #include <map>
 #include <string>
 #include <vector>
+#include <iostream>
 
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/ini_parser.hpp>
@@ -26,6 +27,7 @@ public:
         {"rbd_volume_name","volume_1"},
         {"cache_dir","/tmp/"},
         {"cache_mode","write_back"},
+        {"policy_mode","tier"},
         {"total_size","10737418240"},
         {"cache_total_size","10737418240"},
         {"cache_ratio_health","0.85"},
